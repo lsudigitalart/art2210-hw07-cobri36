5b0c7c63156dcd63dded0c68e9458d02ed6a9ca1
casobri17 <casobri17@gmail.com>
2024-11-26 00:23:14 -0600

    huyhi

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+function setup() {
+    createCanvas(600, 600);
+    textAlign(CENTER, CENTER);
+    textSize(32);
+}
+
 function draw() {
     background(57, 225, 20); // Set the background color to green
-    fill(0); // Set the fill color to black
-    ellipse(width / 2, height / 2, 100, 100); // Draw a circle in the center
+    fill(0); // Set text color to black
+    text("Hello, World!", width / 2, height / 2); // Display text in the center
 }
